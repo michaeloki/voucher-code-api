c6b842f9bddc24f9abc59a9fc7c8c38b52e54d78
Michael Oki <mikeoki.oki@gmail.com>
2023-05-10 17:57:10 +0200

    Added index to the components dir

--- Frontend/voucher-ui/src/app/app.module.ts
+++ Frontend/voucher-ui/src/app/app.module.ts
@@ -2,7 +2,7 @@ import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
-import { IndexComponent } from './index/index.component';
+import { IndexComponent } from './components/index/index.component';
 import {HttpClientModule} from "@angular/common/http";
 import {BrowserAnimationsModule} from "@angular/platform-browser/animations";
 import {FormsModule} from "@angular/forms";
